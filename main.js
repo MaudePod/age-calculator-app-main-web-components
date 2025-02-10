2a3c26cd55a12f438b88778dfe806f4879aa35d1
MaudePod <186677756+MaudePod@users.noreply.github.com>
2025-02-10 23:21:57 +0000

    adds event listeners

--- main.js
+++ main.js
@@ -1,23 +1,65 @@
-export default class AgeCalculatorComponent extends HTMLElement {
+class AgeCalculatorComponent extends HTMLElement {
     #internals;
-    #dayMonths = {
-        "January": 31,
-        "February": 28,
-        "March": 31,
-        "April": 30,
-        "May": 31,
-        "June": 30,
-        "July": 31,
-        "August": 31,
-        "September": 30,
-        "October": 31,
-        "November": 30,
-        "December": 31
-    }
+    #dayMonths = [
+        {
+            month: "January", days: 31
+        },
+        {
+            month: "February", days: 28
+        },
+        {
+            month: "March", days: 31
+        },
+        {
+            month: "April", days: 30
+        },
+        {
+            month: "May", days: 31
+        },
+        {
+            month: "June", days: 30
+        },
+        {
+            month: "July", days: 31
+        },
+        {
+            month: "August", days: 31
+        },
+        {
+            month: "September", days: 30
+        },
+        {
+            month: "October", days: 31
+        },
+        {
+            month: "November", days: 30
+        },
+        {
+            month: "December", days: 31
+        }
+    ];
+    #form;
+    #dayInput;
+    #monthInput;
+    #yearInput;
     constructor() {
         super();
     };
-    connectedCallback() { }
+    connectedCallback() {
+        this.#internals = this.attachInternals();
+        this.#form = this.#internals.shadowRoot.querySelector("form");
+        this.#dayInput=this.#internals.shadowRoot.querySelector("input[id='day']");
+        this.#monthInput=this.#internals.shadowRoot.querySelector("input[id='month']");
+        this.#yearInput=this.#internals.shadowRoot.querySelector("input[id='year']").max = new Date().getFullYear();  
+        this.#monthInput.addEventListener('input', (event) => {
+            const lastDayInAGivenMonth = this.#dayMonths[this.#monthInput.value - 1].days;
+            this.setMaximumValidDate(lastDayInAGivenMonth);
+        });
+        this.#form.addEventListener('input', (event) => this.#form.reportValidity());
+    }
+    setMaximumValidDate = (lastDayInAGivenMonth) => {
+        this.#dayInput.max = lastDayInAGivenMonth;
+    }
     disconnectedCallback() { }
 }
 if (!customElements.get("age-calculator-component")) {
